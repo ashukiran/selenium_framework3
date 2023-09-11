25274d8a8b475f4ca7971dc105a5da147016e209
ashwini rao <ashwini rao@Ashukiran>
2023-09-11 13:02:51 +0530

    3rd push

---
+++ practice_framework/src/test/java/practice_framework/dummY_class.java
@@ -0,0 +1,14 @@
+package practice_framework;
+
+import org.testng.annotations.Test;
+
+@Test
+public class dummY_class {
+	public void m2() {
+		System.out.println("hii");
+	}
+	public void m1() {
+		System.out.println("hello");
+	}
+
+}
